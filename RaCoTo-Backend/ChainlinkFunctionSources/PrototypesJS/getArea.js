2ebed8e3ad6c3c73d06564e94834f3bb17c3a9ee
Arun89-crypto <arunjngra89@gmail.com>
2023-06-09 23:49:32 +0530

    Revert "feat: added code for FE and BE"
    This reverts commit 05c11d7fe5fd5fc27725e6a3c24907ae373c3c71.

--- RaCoTo-Backend/ChainlinkFunctionSources/PrototypesJS/getArea.js
+++
@@ -1,6 +0,0 @@
-async function getArea(geoStore = "f3ceac63-34cb-e9d6-e436-f0c42cafff10") {
-  const gfwBaseURL = "https://data-api.globalforestwatch.org/" // Make sure to define the gfwBaseURL
-  const requestedData = await fetch(`${gfwBaseURL}geostore/${geoStore}`)
-  const json = await requestedData.json()
-  return json["data"]["gfw_area__ha"]
-}
--- RaCoTo-Backend/contracts/Interface/Governance/IRaCoToUserGov.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
-
-interface IRaCoToUserGov {
-  function isUserWhitelisted(address _owner) external view returns (bool);
-
-  function getgeoJSON(address _owner) external view returns (string memory);
-}
